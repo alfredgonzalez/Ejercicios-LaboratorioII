806d90edf645c042e58b7368c3f1428a9260335c
AlfredoGonzalez <alfredoundav@gmail.com>
2022-09-12 22:13:21 -0300

    Ejercicio clase 3

---
+++ Ejercicio 3 colecciones/Ejercicio 3 colecciones/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Ejercicio_3_colecciones
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rchTexto = new System.Windows.Forms.RichTextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rchTexto
+            // 
+            this.rchTexto.Location = new System.Drawing.Point(12, 12);
+            this.rchTexto.Name = "rchTexto";
+            this.rchTexto.Size = new System.Drawing.Size(532, 461);
+            this.rchTexto.TabIndex = 0;
+            this.rchTexto.Text = "";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(450, 493);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(94, 29);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 534);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.rchTexto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rchTexto;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
---
+++ Ejercicio 3 colecciones/Ejercicio 3 colecciones/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_3_colecciones
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, int> texto;
+        List<string> listaPalabras;
+        public Form1()
+        {
+            InitializeComponent();
+            texto = new Dictionary<string, int>();
+            listaPalabras = new List<string>();
+        }
+
+        public void ContarPalabras(string texto) 
+        {
+            listaPalabras.AddRange(texto.Split(' ',',',';','.','\t','\n'));
+
+            foreach(string palabra in listaPalabras) 
+            {
+                if (!this.texto.ContainsKey(palabra)) 
+                {
+                    this.texto.Add(palabra,1);
+                }
+                else 
+                {
+                    this.texto[palabra] += 1;
+                }
+            }
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            ContarPalabras(rchTexto.Text);
+            foreach (KeyValuePair<string, int> elemento in this.texto) 
+            {
+                sb.AppendLine($"{elemento.Key}\t{elemento.Value}" );
+            }
+            MessageBox.Show(sb.ToString(), "Texto");
+        }
+    }
+}
