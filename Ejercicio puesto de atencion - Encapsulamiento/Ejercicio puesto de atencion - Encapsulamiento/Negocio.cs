0c21865c9721e72d37137d04fe39a1e97508986c
AlfredoGonzalez <alfredoundav@gmail.com>
2022-09-13 21:45:41 -0300

    Ejercicio 1309

---
+++ Ejercicio puesto de atencion - Encapsulamiento/Ejercicio puesto de atencion - Encapsulamiento/Negocio.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Negocio
+    {
+        private PuestoDeAtencion caja;
+        private Queue<Cliente> clientes;
+        private string nombre;
+
+        public Cliente Cliente { get { return this.clientes.Peek();} set {; } }
+
+        private Negocio() 
+        {
+            clientes = new Queue<Cliente>();
+            caja = new PuestoDeAtencion(Puesto.Caja1);
+        }
+        public Negocio(string nombre) :this()
+        {
+            this.nombre = nombre;
+        }
+        public static bool operator !=(Negocio n, Cliente c) 
+        {
+            return !(n == c);
+        }
+        public static bool operator ==(Negocio n, Cliente c) 
+        {
+            foreach(Cliente item in n.clientes) 
+            {
+                if(item == c) 
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool operator +(Negocio n, Cliente c) 
+        {
+            if(n!= c) 
+            {
+                n.clientes.Enqueue(c);
+                return true;
+            }
+            return false;
+        }
+        public static bool operator ~(Negocio n) 
+        {
+            if(n is not null && n.clientes.Count > 0)
+            {
+                n.caja.Atender(n.Cliente);
+                n.clientes.Dequeue();
+                return true;
+              
+            }
+            return false;
+        }
+
+        public int ClientesPendientes { get { return this.clientes.Count; } }
+    }
+}
---
+++ Ejercicio puesto de atencion - Encapsulamiento/Ejercicio puesto de atencion - Encapsulamiento/PuestoDeAtencion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public enum Puesto { Caja1, Caja2 }
+    public class PuestoDeAtencion
+    {
+        static int numeroActual;
+        private Puesto puesto;
+
+        public int NumeroActual { get { return numeroActual+1; }  }
+
+        public bool Atender(Cliente c) 
+        {
+           if(c is not null) 
+            {
+                Thread.Sleep(3000);
+                return true;
+            }
+            return false;
+        }
+        static PuestoDeAtencion() 
+        {
+            numeroActual = 0;
+        }
+        public PuestoDeAtencion(Puesto puesto)
+        {
+            this.puesto = puesto;
+        }
+        
+
+    }
+}
